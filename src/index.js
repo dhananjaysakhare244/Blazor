491b70b406b61b40ffd4a08eca18a6dc3707c454
Dhananjay Sakhare <dhananjaysakhare244@gmail.com>
2021-04-01 22:49:04 +0530

    before functional component implemented

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Main from './components/Main'
-
+import './styles/stylesheet.css'
 ReactDom.render(<Main></Main>,document.getElementById('root'));
+
+
