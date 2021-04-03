2818948359189e08deed055311e840b6d5ff8d02
Dhananjay Sakhare <dhananjaysakhare244@gmail.com>
2021-04-03 13:31:26 +0530

    47.Reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDom from "react-dom";
 import Main from "./components/Main";
 import "./styles/stylesheet.css";
 import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
+import rootReducer from "./redux/reducer";
+
+const store = createStore(rootReducer);
+
 ReactDom.render(
   <BrowserRouter>
     <Main />
