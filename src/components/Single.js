2e70a4104e233e95f0091f7264e3b0dce46ddf0a
Dhananjay Sakhare <dhananjaysakhare244@gmail.com>
2021-04-06 17:16:44 +0530

    index bug fixed

--- src/components/Single.js
+++ src/components/Single.js
@@ -6,9 +6,9 @@ class Single extends Component {
     const { match, posts } = this.props;
     const id = Number(match.params.id);
     const post = posts.find((post) => post.id === id);
-    const comments = this.props.comments[id] || [];
-    console.log(this.props.comments);
-    const index = this.props.posts.findIndex((post) => posts.id === id);
+    const comments = this.props.comments[match.params.id] || [];
+    const index = this.props.posts.findIndex((post) => post.id === id);
+
     return (
       <div className="single-photo">
         <Photo post={post} {...this.props} index={index} />
