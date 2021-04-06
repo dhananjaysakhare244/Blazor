d8a0913bbb52a3938a6f5dd0f964223c5df5e224
Dhananjay Sakhare <dhananjaysakhare244@gmail.com>
2021-04-06 15:56:09 +0530

    56 UI setup

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import Title from "./Title";
 import PhotoWall from "./PhotoWall";
 import AddPhoto from "./AddPhoto";
 import { Route } from "react-router-dom";
 import { Link } from "react-router-dom";
+import Single from "./Single";
 //array
 class Main extends Component {
   render() {
@@ -28,6 +28,10 @@ class Main extends Component {
           path="/addPhoto"
           render={() => <AddPhoto {...this.props} />}
         />
+        <Route
+          path="/single/:id"
+          render={(params) => <Single {...this.props} {...params} />}
+        ></Route>
       </div>
     );
   }
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 class Photo extends Component {
   render() {
     const post = this.props.post;
     return (
       <figure className="figure">
-        <img
-          src={post.imageLink}
-          alt={post.description}
-          className="photo"
-        ></img>
+        <Link to={`/single/${post.id}`}>
+          <img
+            src={post.imageLink}
+            alt={post.description}
+            className="photo"
+          ></img>
+        </Link>
+
         <figcaption>
           <p>{post.description}</p>
         </figcaption>
