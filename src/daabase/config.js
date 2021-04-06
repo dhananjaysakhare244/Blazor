472765832c0b3f9e6db2a99b125b5d8c2138663a
Dhananjay Sakhare <dhananjaysakhare244@gmail.com>
2021-04-06 23:39:43 +0530

    firebase incomplete

---
+++ src/daabase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDEY4CjKiBVekme0RqQZ5zVNQH70TqZaTc",
+  authDomain: "photowall-11f0e.firebaseapp.com",
+  databaseURL: "https://photowall-11f0e-default-rtdb.firebaseio.com",
+  projectId: "photowall-11f0e",
+  storageBucket: "photowall-11f0e.appspot.com",
+  messagingSenderId: "804367290190",
+  appId: "1:804367290190:web:cf5e0aacf0b178339f181f",
+  measurementId: "G-5MJQFW5W7F",
+};
